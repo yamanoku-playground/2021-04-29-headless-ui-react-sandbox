5e91be5f994ef1224170d8de587fc3a131e127e4
yamanoku <0910yama@gmail.com>
2021-04-29 18:58:21 +0900

    add: Class

--- src/Disclosure.js
+++ src/Disclosure.js
@@ -2,31 +2,50 @@ import { Disclosure, Transition } from "@headlessui/react";
 
 function MyDisclosure() {
   return (
-    <Disclosure>
-      {({ open }) => (
-          <>
-            <Disclosure.Button>
+    <div className="w-full px-4 pt-16">
+      <div className="w-full max-w-md p-2 mx-auto bg-white rounded-2xl">
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                 Is team pricing available?
-            </Disclosure.Button>
-            {open && (
-          <Transition
-          show={open}
-          enter="transition duration-100 ease-out"
-          enterFrom="transform scale-95 opacity-0"
-          enterTo="transform scale-100 opacity-100"
-          leave="transition duration-75 ease-out"
-          leaveFrom="transform scale-100 opacity-100"
-          leaveTo="transform scale-95 opacity-0"
-          >
-            <Disclosure.Panel static>
-                Yes! You can purchase a license that you can share with your entire
-                team.
-            </Disclosure.Panel>
-            </Transition>
-        )}
-          </>
-      )}
-    </Disclosure>
+              </Disclosure.Button>
+              {open && (
+                <Transition
+                  show={open}
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Disclosure.Panel
+                    static
+                    className="px-4 pt-4 pb-2 text-sm text-gray-500"
+                  >
+                    Yes! You can purchase a license that you can share with your
+                    entire team.
+                  </Disclosure.Panel>
+                </Transition>
+              )}
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span>Do you offer technical support?</span>
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                No.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </div>
+    </div>
   );
 }
 
